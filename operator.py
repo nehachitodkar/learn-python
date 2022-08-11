ad36c518c39bbeeb0ed9144eeddff816cd60b0fb
Neha Chitodkar <110806013+nehachitodkar@users.noreply.github.com>
2022-08-11 16:40:35 +0530

    Add program for operator in python

---
+++ operator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+x = 15
+y = 4
+
+print('x + y =',x +y)
+print('x - y =',x - y)
+print('x * y =',x * y)
+print('x / y =',x / y)
+print('x // y =',x // y)
+print('x ** y =',x ** y)
+
+#print('x > y is'x > y)
+#print('x < y is'x < y)
+
+
+
