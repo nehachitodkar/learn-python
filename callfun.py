7df4822f9351ae2c9f26b04eff623b874f362f4e
Neha Chitodkar <110806013+nehachitodkar@users.noreply.github.com>
2022-08-12 17:15:30 +0530

    Add function call python program

---
+++ callfun.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+def my_function():
+    print("Hello From Neha!")
+
+def my_fun_with_args(username, greeting):
+    print("Hello, %s, From Neha!, I wish you %s"%(username, greeting))
+def sum_of_two_numbers(a, b):
+    return a+b
+
+my_function()
+
+my_fun_with_args("John Doe","a great year!")
+
+x = sum_of_two_numbers(1, 2)
+
