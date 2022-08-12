25a180263f80015888b48f54ce5f788ab6acddf1
Neha Chitodkar <110806013+nehachitodkar@users.noreply.github.com>
2022-08-12 16:15:52 +0530

    Add nestedif python program

---
+++ nestedif.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+num = float(input("Enter a number: "))
+
+if num >= 0:
+    if num == 0:
+        print("zero")
+    else:
+        print("Positive number")
+else:
+    print("Negative number")
+
+
